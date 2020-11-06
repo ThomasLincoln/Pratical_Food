44196a9031bb715e69260dac704d6d17860ad35d
Thomas Lincoln <thmas5952@gmail.com>
2020-11-06 14:34:03 -0300

    .

--- Pratical_Food/lib/screens/HomePage.dart
+++ Pratical_Food/lib/screens/HomePage.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:cloud_firestore/cloud_firestore.dart';
-=======
-import 'package:cloud_firestore/cloud_firestore.dart';q
->>>>>>> login
 
 
 class HomePage extends StatefulWidget {
